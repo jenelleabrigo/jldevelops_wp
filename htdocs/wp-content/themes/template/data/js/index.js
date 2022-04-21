60926565df72c8f3c31d3503efeca673cd386e5b
jenelleabrigo <jenelleabrigo@gmail.com>
2022-04-21 22:39:07 +0800

    add techstack logo add works modal

--- htdocs/wp-content/themes/template/data/js/index.js
+++ htdocs/wp-content/themes/template/data/js/index.js
@@ -19,13 +19,34 @@
   })
 
   //header on scroll
-  $(window).scroll(function(){
-    if ($(window).scrollTop() > $(".p-index__carousel").height()) {
-      $(".l-header").addClass("is-scrolled");
-    }
-    else {
-      $(".l-header").removeClass("is-scrolled");
-    }
+  // $(window).scroll(function(){
+  //   if ($(window).scrollTop() > $(".p-index__carousel").height()) {
+  //     $(".l-header").addClass("is-scrolled");
+  //   }
+  //   else {
+  //     $(".l-header").removeClass("is-scrolled");
+  //   }
+  // })
+
+  //modal
+  $(function(){
+    var modal = $('.p-index__works__inner__modal');
+    var item = $('.p-index__works__inner__item');
+    var close = $('.p-index__works__inner__modal__close');
+
+    item.on("click", function(){
+      $(this).siblings().css("display", "block");
+    })
+
+    close.on("click", function(){
+      $(this).parent(modal).css("display", "none");
+    })
+
+    $(window).click(function(e) {
+      if(e.target.className === 'p-index__works__inner__modal'){
+        modal.css("display", "none");
+      };
+    });
   })
 
 })(jQuery);
--- htdocs/wp-content/themes/template/page-home.php
+++ htdocs/wp-content/themes/template/page-home.php
@@ -15,33 +15,82 @@
       <h2 class="c-headline u-subheading u-font-serif">WORKS</h2>
 
       <div class="p-index__works__inner">
-        <a href="#" class="p-index__works__inner__item">
-          <div class="p-index__works__inner__item__img-container">
-            <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+        <div class="p-index__works__inner__item-container">
+          <div class="p-index__works__inner__item">
+            <div class="p-index__works__inner__item__img-container">
+              <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+            </div>
+            <div class="p-index__works__inner__item__text-container">
+              <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
+              <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+            </div>
           </div>
-          <div class="p-index__works__inner__item__text-container">
-            <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
-            <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+          <div class="p-index__works__inner__modal">
+            <span class="p-index__works__inner__modal__close">&times;</span>
+            <div class="p-index__works__inner__modal__content">
+              <h1>this is my modal 1</h1>
+            </div>
           </div>
-        </a>
-        <a href="#" class="p-index__works__inner__item">
-          <div class="p-index__works__inner__item__img-container">
-            <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+        </div>
+        <div class="p-index__works__inner__item-container">
+          <div class="p-index__works__inner__item">
+            <div class="p-index__works__inner__item__img-container">
+              <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+            </div>
+            <div class="p-index__works__inner__item__text-container">
+              <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
+              <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+            </div>
           </div>
-          <div class="p-index__works__inner__item__text-container">
-            <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
-            <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+          <div class="p-index__works__inner__modal">
+            <span class="p-index__works__inner__modal__close">&times;</span>
+            <div class="p-index__works__inner__modal__content">
+              <h1>this is my modal 2</h1>
+            </div>
           </div>
-        </a>
-        <a href="#" class="p-index__works__inner__item">
-          <div class="p-index__works__inner__item__img-container">
-            <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+        </div>
+        <div class="p-index__works__inner__item-container">
+          <div class="p-index__works__inner__item">
+            <div class="p-index__works__inner__item__img-container">
+              <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+            </div>
+            <div class="p-index__works__inner__item__text-container">
+              <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
+              <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+            </div>
           </div>
-          <div class="p-index__works__inner__item__text-container">
-            <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
-            <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+          <div class="p-index__works__inner__modal">
+            <span class="p-index__works__inner__modal__close">&times;</span>
+            <div class="p-index__works__inner__modal__content">
+              <h1>this is my modal 3</h1>
+            </div>
           </div>
-        </a>
+        </div>
+        <div class="p-index__works__inner__item-container">
+          <div class="p-index__works__inner__item">
+            <div class="p-index__works__inner__item__img-container">
+              <img src="<?php echo get_template_directory_uri();?>/data/img/works/img-sample.jpg" alt="project1" class="p-index__works__inner__item__img-container__img">
+            </div>
+            <div class="p-index__works__inner__item__text-container">
+              <p class="p-index__works__inner__item__text-container__title u-text is-big u-font-serif">PROJECT 1</p>
+              <p class="p-index__works__inner__item__text-container__description u-text is-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras malesuada posuere massa, non viverra tellus euismod a. Morbi consequat blandit sem, et luctus leo consequat id.</p>
+            </div>
+          </div>
+          <div class="p-index__works__inner__modal">
+            <span class="p-index__works__inner__modal__close">&times;</span>
+            <div class="p-index__works__inner__modal__content">
+              <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mattis elit sed ligula iaculis, in fringilla ipsum suscipit. In in elementum mauris. Etiam a porttitor ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc nec ante consequat, pharetra massa volutpat, facilisis augue. Donec volutpat urna eget enim sagittis, non lobortis nisi euismod. Nulla bibendum ligula sed lacinia posuere. Vestibulum interdum consequat aliquet. Vivamus convallis mollis metus, ac sollicitudin purus tempor vitae. Phasellus porta felis in dolor gravida ultrices. Maecenas id tincidunt arcu. Proin tincidunt ipsum quis leo vehicula tempor. Etiam varius ipsum nec urna iaculis facilisis.
+
+Suspendisse commodo ultrices erat, et venenatis metus varius a. Praesent hendrerit eget lectus sed efficitur. Nam mattis fermentum placerat. Pellentesque ultrices sem sed pretium molestie. Curabitur quis tempor enim, sit amet blandit lacus. Mauris ut orci at arcu dapibus lacinia sodales eget tortor. Donec scelerisque eu dolor sed condimentum. Praesent rutrum eu orci a egestas.
+
+Pellentesque et laoreet mauris. Donec non justo porttitor, porta erat id, dapibus mi. Donec ullamcorper quam sit amet nisi dignissim, vel lobortis nisi efficitur. Pellentesque at neque sem. Integer maximus purus non magna molestie maximus. Suspendisse placerat condimentum aliquet. Morbi egestas erat nec ultricies consequat. Aliquam turpis mauris, ullamcorper vel tellus eu, finibus imperdiet nunc. Integer in volutpat ex. Donec consectetur pellentesque commodo.
+
+Curabitur placerat lobortis ultrices. Suspendisse potenti. Vestibulum ex elit, vestibulum id ultricies vel, egestas non sem. Proin vel varius turpis. Vestibulum pulvinar ligula vel pulvinar semper. Nam nisl ex, dictum ac felis eu, commodo accumsan ante. Quisque feugiat tellus nec est maximus, ut dapibus nibh tempus. Fusce auctor diam vitae lobortis dignissim. Nulla sit amet massa quis leo faucibus lobortis sit amet molestie enim. Quisque aliquam elementum sapien. Praesent dapibus laoreet ullamcorper.
+
+Integer enim velit, rhoncus at sapien a, porttitor consectetur sapien. Maecenas sit amet tincidunt nisl. Maecenas nec dignissim metus, in iaculis nisi. Donec in mattis mi. Pellentesque sagittis, leo quis finibus pellentesque, metus neque faucibus est, id suscipit dui libero id dolor. Quisque pretium purus enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut aliquet, mauris quis interdum sollicitudin, lorem lorem consequat est, vitae interdum eros augue in dolor. Sed suscipit porttitor erat, eu posuere mauris rutrum eget. Proin accumsan maximus euismod. Sed tristique mi in velit cursus, vitae consequat sapien maximus. Morbi fermentum porttitor dui ac lobortis. Maecenas ac varius mi. Etiam pulvinar, ante et consectetur imperdiet, nulla augue pharetra tellus, vitae tristique libero justo eget ligula.</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -54,6 +103,14 @@
           <tr class="p-index__journey__inner__table__row u-text is-small">
             <th class="p-index__journey__inner__table__date">SEPTEMBER 2021</th>
             <td class="p-index__journey__inner__table__details">I started reading about MySQL in a well-known web development tutorial website w3schools. This was essential to me since we somehow use this tool in my work as Process Engineer in a korean manufacturing company. My reading became a habit by then so I also started reading about HTML and CSS out of curiosity about Web Developing in the same learning site.</td>
+          </tr>
+          <tr class="p-index__journey__inner__table__row u-text is-small">
+            <th class="p-index__journey__inner__table__date">SEPTEMBER 2021</th>
+            <td class="p-index__journey__inner__table__details">I started reading about MySQL in a well-known web development tutorial website w3schools. This was essential to me since we somehow use this tool in my work as Process Engineer in a korean manufacturing company. </td>
+          </tr>
+          <tr class="p-index__journey__inner__table__row u-text is-small">
+            <th class="p-index__journey__inner__table__date">SEPTEMBER 2021</th>
+            <td class="p-index__journey__inner__table__details">I started reading about</td>
           </tr>
         </table>
       </div>
